1f2bac40db378476e70945649cff10b75e4cdafd
Tommi Reiman <tommi@iki.fi>
2017-12-03 17:43:57 +0200

    example of just coercion with ring

---
+++ examples/just-coercion-with-ring/src/example/dspec.clj
@@ -0,0 +1,15 @@
+(ns example.dspec
+  (:require [reitit.ring.coercion :as coercion]
+            [reitit.ring.coercion.spec :as spec-coercion]
+            [example.server :as server]))
+
+(defn handler [{{{:keys [x y]} :query} :parameters}]
+  {:status 200
+   :body {:result (+ x y)
+          :source :data-spec}})
+
+(def app
+  (-> #'handler
+      (server/wrap-coercion
+        {:parameters {:query {:x int?, :y int?}}
+         :coercion spec-coercion/coercion})))
---
+++ examples/just-coercion-with-ring/src/example/server.clj
@@ -0,0 +1,28 @@
+(ns example.server
+  (:require [ring.adapter.jetty :as jetty]
+            [reitit.ring.middleware :as middleware]
+            [reitit.ring.coercion :as coercion]))
+
+(defonce ^:private server (atom nil))
+
+;; unlift Middleware Record into vanilla Ring middleware
+;; NOTE: to support format-based body coercion, an options map needs
+;; to be set with :extract-request-format and extract-response-format
+(defn wrap-coercion [handler resource]
+  (middleware/chain
+    [coercion/coerce-request-middleware
+     coercion/coerce-response-middleware
+     coercion/coerce-exceptions-middleware]
+    handler
+    resource))
+
+(defn restart [handler]
+  (let [app (-> handler
+                (ring.middleware.params/wrap-params)
+                (muuntaja.middleware/wrap-format))]
+    (swap! server (fn [x]
+                    (when x (.stop x))
+                    (jetty/run-jetty
+                      handler
+                      {:port 3000, :join? false})))
+    (println "server running in port 3000")))
