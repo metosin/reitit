18bd2e684f0ad8a8a9dc60fa1ef7e67d4d08e0a2
Tommi Reiman <tommi@iki.fi>
2018-06-24 22:32:23 +0300

    bump up version (all but docs)

--- modules/reitit-swagger/project.clj
+++ modules/reitit-swagger/project.clj
@@ -1,4 +1,4 @@
-(defproject metosin/reitit-swagger "0.1.2"
+(defproject metosin/reitit-swagger "0.1.3-SNAPSHOT"
   :description "Reitit: Swagger-support"
   :url "https://github.com/metosin/reitit"
   :license {:name "Eclipse Public License"
