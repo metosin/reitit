d54c05426ccf089a81b2842675eb640ca66955ea
Juho Teperi <juho.teperi@iki.fi>
2018-07-24 11:45:51 +0300

    Share router between history tests

--- test/cljs/reitit/frontend/history_test.cljs
+++ test/cljs/reitit/frontend/history_test.cljs
@@ -1,21 +1,20 @@
 (ns reitit.frontend.history-test
   (:require [clojure.test :refer [deftest testing is are]]
             [reitit.core :as r]
+            [reitit.frontend :as rf]
             [reitit.frontend.history :as rfh]
             [reitit.frontend.test-utils :refer [capture-console]]))
 
 (def browser (exists? js/window))
 
+(def router (r/router ["/"
+                       ["" ::frontpage]
+                       ["foo" ::foo]
+                       ["bar/:id" ::bar]]))
+
 (deftest fragment-history-test
   (when browser
-    (let [router (r/router ["/"
-                            ["" ::frontpage]
-                            ["foo" ::foo]
-                            ["bar/:id" ::bar]])
-          history (rfh/start! router
-                              (fn [_])
-                              {:use-fragment true
-                               :path-prefix "/"})]
+    (let [history (rfh/start! router (fn [_]) {:use-fragment true})]
 
       (testing "creating urls"
         (is (= "#/foo"
@@ -34,14 +33,7 @@
 
 (deftest html5-history-test
   (when browser
-    (let [router (r/router ["/"
-                            ["" ::frontpage]
-                            ["foo" ::foo]
-                            ["bar/:id" ::bar]])
-          history (rfh/start! router
-                              (fn [_])
-                              {:use-fragment false
-                               :path-prefix "/"})]
+    (let [history (rfh/start! router (fn [_]) {:use-fragment false})]
 
       (testing "creating urls"
         (is (= "/foo"
