98707902ea4187fe2c2db0a705e4b77efa0e6558
Juho Teperi <juho.teperi@iki.fi>
2018-09-04 19:21:07 +0300

    Add :scm tags for clojars commit link

--- modules/reitit-ring/project.clj
+++ modules/reitit-ring/project.clj
@@ -3,6 +3,8 @@
   :url "https://github.com/metosin/reitit"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :scm {:name "git"
+        :url "https://github.com/metosin/reitit"}
   :plugins [[lein-parent "0.3.2"]]
   :parent-project {:path "../../project.clj"
                    :inherit [:deploy-repositories :managed-dependencies]}
