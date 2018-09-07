9260f3b3fd19c75e92ab0b94dc911682296fcdb6
Tommi Reiman <tommi@iki.fi>
2018-09-08 00:16:59 +0300

    pedestal+reitit-http+swagger

---
+++ examples/pedestal-swagger/project.clj
@@ -0,0 +1,7 @@
+(defproject ring-example "0.1.0-SNAPSHOT"
+  :description "Reitit-http with pedestal"
+  :dependencies [[org.clojure/clojure "1.9.0"]
+                 [io.pedestal/pedestal.service "0.5.4"]
+                 [io.pedestal/pedestal.jetty "0.5.4"]
+                 [metosin/reitit "0.2.1"]]
+  :repl-options {:init-ns example.server})
