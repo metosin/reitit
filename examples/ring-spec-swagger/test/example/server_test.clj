fb91499efdf62e46bf1bfc1c340c2a29e6fd4e59
Johannes Lötzsch <git@johannesloetzsch.de>
2018-09-23 22:48:32 +0200

    test whole stack (use query-string and json-body)

--- examples/ring-spec-swagger/test/example/server_test.clj
+++ examples/ring-spec-swagger/test/example/server_test.clj
@@ -1,16 +1,23 @@
 (ns example.server-test
   (:require [clojure.test :refer :all]
-            [example.server :refer [app]]))
+            [example.server :refer [app]]
+            [ring.mock.request :refer [request json-body]]))
 
 (deftest example-server
 
   (testing "GET"
-    (is (= (-> {:request-method :get :uri "/math/plus" :query-params {:x 20 :y 3}}
+    (is (= (-> (request :get "/math/plus?x=20&y=3")
+               app :body slurp)
+           (-> {:request-method :get :uri "/math/plus" :query-string "x=20&y=3"}
+               app :body slurp)
+           (-> {:request-method :get :uri "/math/plus" :query-params {:x 20 :y 3}}
                app :body slurp)
            "{\"total\":23}")))
 
   (testing "POST"
-    (is (= (-> {:request-method :post :uri "/math/plus" :body-params {:x 40 :y 2}}
+    (is (= (-> (request :post "/math/plus") (json-body {:x 40 :y 2})
+               app :body slurp)
+           (-> {:request-method :post :uri "/math/plus" :body-params {:x 40 :y 2}}
                app :body slurp)
            "{\"total\":42}")))
 
