c0201ce2f015b64225a22d9d764aceb221bc58ca
Tommi Reiman <tommi@iki.fi>
2018-09-24 20:27:42 +0300

    test parameters-interceptor

--- test/clj/reitit/http/interceptors/parameters_test.clj
+++ test/clj/reitit/http/interceptors/parameters_test.clj
@@ -1,3 +1,17 @@
-(ns reitit.http.interceptors.parameters-test)
+(ns reitit.http.interceptors.parameters-test
+  (:require [clojure.test :refer [deftest testing is]]
+            [reitit.http.interceptors.parameters :as parameters]
+            [reitit.http :as http]
+            [reitit.interceptor.sieppari :as sieppari]))
 
-;; TODO
+(deftest parameters-test
+  (let [app (http/ring-handler
+              (http/router
+                ["/ping" {:get #(select-keys % [:params :query-params])}]
+                {:data {:interceptors [(parameters/parameters-interceptor)]}})
+              {:executor sieppari/executor})]
+    (is (= {:query-params {"kikka" "kukka"}
+            :params {"kikka" "kukka"}}
+           (app {:request-method :get
+                 :uri "/ping"
+                 :query-string "kikka=kukka"})))))
