8fb6582b67dc82883637b4ea0809615d0894ae72
Tommi Reiman <tommi@iki.fi>
2018-10-21 23:10:50 +0300

    Update jetty-adapter

--- examples/http/project.clj
+++ examples/http/project.clj
@@ -4,6 +4,6 @@
                  [org.clojure/core.async "0.4.474"]
                  [funcool/promesa "1.9.0"]
                  [manifold "0.1.8"]
-                 [ring/ring-jetty-adapter "1.7.0-RC2"]
+                 [ring/ring-jetty-adapter "1.7.0"]
                  [metosin/reitit "0.2.4"]]
   :repl-options {:init-ns example.server})
