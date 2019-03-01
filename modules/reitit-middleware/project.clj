680414a2095777323640442950229d03d1348ca3
Miikka Koskinen <miikka@metosin.fi>
2019-03-01 13:20:43 +0200

    Point each project.clj to the git directory
    This way the released JARs should include the git commit they were built
    from.

--- modules/reitit-middleware/project.clj
+++ modules/reitit-middleware/project.clj
@@ -4,7 +4,8 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :scm {:name "git"
-        :url "https://github.com/metosin/reitit"}
+        :url "https://github.com/metosin/reitit"
+        :scm "../.."}
   :plugins [[lein-parent "0.3.2"]]
   :parent-project {:path "../../project.clj"
                    :inherit [:deploy-repositories :managed-dependencies]}
