96c2764de6ce3b46c97ff837d6db5b591c98d93b
Tommi Reiman <tommi@iki.fi>
2020-02-14 16:27:45 +0200

    Update server.clj

--- examples/ring-malli-swagger/src/example/server.clj
+++ examples/ring-malli-swagger/src/example/server.clj
@@ -55,13 +55,13 @@
         {:swagger {:tags ["math"]}}
 
         ["/plus"
-         {:get {:summary "plus with spec query parameters"
+         {:get {:summary "plus with malli query parameters"
                 :parameters {:query [:map [:x int?] [:y int?]]}
                 :responses {200 {:body [:map [:total int?]]}}
                 :handler (fn [{{{:keys [x y]} :query} :parameters}]
                            {:status 200
                             :body {:total (+ x y)}})}
-          :post {:summary "plus with spec body parameters"
+          :post {:summary "plus with malli body parameters"
                  :parameters {:body [:map [:x int?] [:y int?]]}
                  :responses {200 {:body [:map [:total int?]]}}
                  :handler (fn [{{{:keys [x y]} :body} :parameters}]
