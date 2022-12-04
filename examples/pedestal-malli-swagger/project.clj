eebc2a3df07c831ef71fc0672c6efff6f95cef5f
Ian Fernandez <d.ian.b@live.com>
2022-12-04 00:07:05 +0000

    Add example for Reitit + Pedestal + Malli coercion

---
+++ examples/pedestal-malli-swagger/project.clj
@@ -0,0 +1,9 @@
+(defproject pedestal-malli-swagger-example "0.1.0-SNAPSHOT"
+  :description "Reitit-http with pedestal"
+  :dependencies [[org.clojure/clojure "1.10.0"]
+                 [io.pedestal/pedestal.service "0.5.5"]
+                 [io.pedestal/pedestal.jetty "0.5.5"]
+                 [metosin/reitit-malli "0.5.18"]
+                 [metosin/reitit-pedestal "0.5.18"]
+                 [metosin/reitit "0.5.18"]]
+  :repl-options {:init-ns example.server})
