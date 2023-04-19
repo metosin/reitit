5227e650296590084ec1aca73b76008a38aa6453
Joel Kaasinen <joel.kaasinen@metosin.fi>
2023-04-19 10:56:22 +0300

    doc: OpenAPI3 named examples in examples/http-swagger

--- examples/http-swagger/src/example/server.clj
+++ examples/http-swagger/src/example/server.clj
@@ -112,6 +112,22 @@
                          {:status 200
                           :body {:total (+ x y)}})}
         :post {:summary "plus with data-spec body parameters"
+               ;; OpenAPI3 named examples for request & response
+               :openapi {:requestBody
+                         {:content
+                          {"application/json"
+                           {:examples {"add-one-one" {:summary "1+1"
+                                                      :value {:x 1 :y 1}}
+                                       "add-one-two" {:summary "1+2"
+                                                      :value {:x 1 :y 2}}}}}}
+                         :responses
+                         {200
+                          {:content
+                           {"application/json"
+                            {:examples {"two" {:summary "2"
+                                               :value {:total 2}}
+                                        "three" {:summary "3"
+                                                 :value {:total 3}}}}}}}}
                :parameters {:body {:x int?, :y int?}}
                :responses {200 {:body {:total int?}}}
                :handler (fn [{{{:keys [x y]} :body} :parameters}]
