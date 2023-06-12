a032abc9106906abe1c0ac7a909bea51be67a9e0
Juho Teperi <juho.teperi@iki.fi>
2023-06-12 18:15:53 +0300

    Fix interceptors deps

--- scripts/cljdoc-check.sh
+++ scripts/cljdoc-check.sh
@@ -2,6 +2,11 @@
 
 set -e
 
+# Need pom and jar for analyze local.
+# Need repo version installed to the local m2 for up-to-date dependencies between modules.
+# Install will run jar and pom tasks already.
+./scripts/lein-modules install
+
 for i in modules/*; do
   cd $i
   clojure -J-Dclojure.main.report=stderr -Tcljdoc-analyzer analyze-local
