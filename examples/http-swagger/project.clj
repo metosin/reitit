bdadb0fa8dbb458c31b049d5a07b53039edfa71a
Vedang Manerikar <ved.manerikar@gmail.com>
2023-09-28 21:58:21 +0530

    Update Clojure version of all Swagger Examples to 1.11
    As of 497da675b92d2d50b00feaf3ff662145a04a250d (committed 1st Sept
    2023), `swagger.cljc` uses the `update-vals` function, introduced in
    Clojure 1.11.
    
    Due to this, all the example projects fail to run since the
    `project.clj` specifies Clojure version 1.10.
    
    This commit updates the Clojure version for such examples, fixing the
    problem.
    
    Fixes: #651

--- examples/http-swagger/project.clj
+++ examples/http-swagger/project.clj
@@ -1,6 +1,6 @@
 (defproject ring-example "0.1.0-SNAPSHOT"
   :description "Reitit Http App with Swagger"
-  :dependencies [[org.clojure/clojure "1.10.0"]
+  :dependencies [[org.clojure/clojure "1.11.1"]
                  [ring/ring-jetty-adapter "1.7.1"]
                  [aleph "0.4.7-alpha5"]
                  [fi.metosin/reitit "0.7.0-alpha6"]
