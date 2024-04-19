1b7fc0fc58fe1ef0994d99798e549593c34df902
Joel Kaasinen <joel.kaasinen@metosin.fi>
2024-04-19 14:30:58 +0300

    deps: bump the deps on some examples

--- examples/ring-spec-swagger/project.clj
+++ examples/ring-spec-swagger/project.clj
@@ -1,8 +1,8 @@
 (defproject ring-example "0.1.0-SNAPSHOT"
   :description "Reitit Ring App with Swagger"
-  :dependencies [[org.clojure/clojure "1.11.1"]
-                 [ring/ring-jetty-adapter "1.7.1"]
+  :dependencies [[org.clojure/clojure "1.11.2"]
+                 [ring/ring-jetty-adapter "1.12.1"]
                  [metosin/reitit "0.7.0-alpha7"]
                  [metosin/ring-swagger-ui "5.9.0"]]
   :repl-options {:init-ns example.server}
-  :profiles {:dev {:dependencies [[ring/ring-mock "0.3.2"]]}})
+  :profiles {:dev {:dependencies [[ring/ring-mock "0.4.0"]]}})
