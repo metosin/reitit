d25dca19f61e80dff648bdaa692e44b3f1275dc2
Joel Kaasinen <joel.kaasinen@metosin.fi>
2025-10-24 16:14:11 +0300

    chore: disable release signing harder

--- project.clj
+++ project.clj
@@ -7,7 +7,7 @@
   :deploy-repositories [["clojars" {:url "https://repo.clojars.org"
                                     :username :env/clojars_username
                                     :password :env/clojars_password
-                                    :sign-release false}]]
+                                    :sign-releases false}]]
   :repositories [["clojars" {:url "https://repo.clojars.org"
                              :username :env/clojars_username
                              :password :env/clojars_password}]]
