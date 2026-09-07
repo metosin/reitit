7121704400918228723e5792d37abf57312d43b6
Karl Pietrzak <karl@medplum.com>
2026-09-07 09:57:14 -0400

    reverting version upgrade
    Signed-off-by: Karl Pietrzak <karl@medplum.com>

--- modules/reitit-openapi/src/reitit/openapi.clj
+++ modules/reitit-openapi/src/reitit/openapi.clj
@@ -212,7 +212,7 @@
            strip-top-level-keys #(dissoc % :id :info :host :basePath :definitions :securityDefinitions)
            strip-endpoint-keys #(dissoc % :id :parameters :responses :summary :description)
            openapi (->> (strip-endpoint-keys openapi)
-                        (merge {:openapi "3.1.0"
+                        (merge {:openapi "3.2.0"
                                 :x-id ids}))
            accept-route (fn [route]
                           (-> route second :openapi :id (or ::default) (trie/into-set) (set/intersection ids) seq))
